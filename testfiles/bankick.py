174be0261ac975133905048b383953dc8e9a19ec
ghostclipz <ghostsammy49@gmail.com>
2022-02-06 18:21:49 +0100

    Update bankick.py

--- testfiles/bankick.py
+++ testfiles/bankick.py
@@ -1,3 +1,5 @@
+from dis import dis
+from email import message
 import discord
 import asyncio
 from discord.ext import commands
@@ -18,12 +20,16 @@ async def status_task():
 @client.command()
 async def kick(ctx, member : discord.Member, *, reason=None):
     await member.kick(reason=reason)
+    embed = discord.Embed(title=f"{member.name} was kicked from the Server!")
+    await ctx.reply(embed=embed)
 
 
 @client.command()
 async def ban(ctx, member : discord.Member, *, reason=None):
     await member.ban(reason=reason)
-
+    embed = discord.Embed(title=f"{member.name} was banned from the Server!")
+    await ctx.reply(embed=embed)
 
 
 client.run("OTM5ODI5MjA2Mjk1MTQyNDAw.Yf-iJw.cuZVaLFfPsGAZZn7vgeg66SuBhA")
+
