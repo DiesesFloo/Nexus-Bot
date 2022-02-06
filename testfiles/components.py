5b45f61191dc8d0670b2733e258df4a5f20b06f4
zflx1 <74198269+zflx1@users.noreply.github.com>
2022-02-06 14:10:22 +0100

    Update components.py

--- testfiles/components.py
+++ testfiles/components.py
@@ -1,3 +1,5 @@
+from turtle import title
+import discord
 from discord import Client, Intents, Embed, Game
 import discord.ext.commands as commands
 from discord_slash import SlashCommand, SlashContext
@@ -7,23 +9,26 @@ client = commands.Bot("-")
 slash = SlashCommand(client, sync_commands=True)
 DiscordComponents(client)
 
-@slash.slash(name="test", description="Just a test", guild_ids=[939533203256000532])
-async def _hello(ctx:SlashContext):
-    await ctx.send("Bestanden!")
+
+@client.event
+async def on_connect():
+    await client.change_presence(activity=discord.Game(name="Python developement"))
+    print("Bot is ready.")
+    print(f"Bot is on {len(client.guilds)} servers.")
 
 @client.command()
-async def hello(ctx):
-    await ctx.send("hello", components= [
-        [Button(label="Website", style=5,emoji="🌐", url="https://diesesfloo.eu/"),
-        Button(label="Check", style=3, emoji="✅", custom_id="sucess"),
-        Button(label="Uncheck", style=4, emoji="❌", custom_id="remove")]
+async def website(ctx):
+    author = ctx.message.author
+    embed = Embed(title="♾️ Website", description=f"Hey <@{author.id}>, \nright now we don't have a website yet, but you can check out the website of our manager Floo.")
+    await ctx.reply(embed=embed, components=[
+        Button(label="Website", style=5, url="http://diesesfloo.eu/")
     ])
-    while True:
-        interaction = await client.wait_for("button_click")
-        if interaction.component.custom_id == "sucess":
-            await interaction.send("✅ Du bist offiziell ein Hurensohn!")
-        elif interaction.component.custom_id == "remove":
-            await interaction.send("✅ Du bist nun kein Hurensohn mehr!")
+
+@client.command()
+async def ping(ctx):
+    author = ctx.message.author
+    embed = Embed(title="♾️ Ping", description=f"🏓 Pong!\nHey, <@{author.id}>, my ping is {round(client.latency * 1000)} ms.")
+    await ctx.reply(embed=embed)
 
 
 client.run("OTM5NTQ4OTk1MDE2MDk3ODAz.Yf6dMA.OCleFdRyflqchx-pgaYtofUMing")
